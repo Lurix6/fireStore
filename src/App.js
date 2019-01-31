68b16e5551828fb91f9365d3a3c4e1d30894d53a
Lurix6 <vasyapopovich23@gmail.com>
2019-01-31 20:12:25 +0200

    cleaning

--- src/App.js
+++ src/App.js
@@ -17,33 +17,20 @@ class App extends Component {
           this.setState({posts: posts})
       })
 
-      console.log('componentDidMount')
     }
 
     componentWillUnmount = () => {
       this.unsubsribe()
-      console.log('componentWillUnmount');
     }
 
 
       handleCreate = async post => {
-        console.log('handleCreate ', this.state);
-        const docRef = await firestore.collection('posts').add(post)
-        const doc = await docRef.get()
-        const newPost = collectIdsAndDocs(doc)
-        this.setState({
-          posts: [newPost ,...this.state.posts]
-        })
+       firestore.collection('posts').add(post)
       }
 
 
         handleDelatePost = async id => {
-          console.log('handleRemove ', this.state);
-          const allPosts = this.state.posts
           await firestore.doc(`posts/${id}`).delete()
-          const posts = allPosts.filter(el => (el.id !== id))
-
-          this.setState({ posts})
         }
 
   render() {
